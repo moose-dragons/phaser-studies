df81fa97aa85f700e70cb64c7820e56adea9f0d6
Kimmo <kimmo.peura@gmail.com>
2016-01-16 19:02:58 +0200

    Trump Moves better

--- src/charmove.js
+++ src/charmove.js
@@ -1,69 +1,97 @@
+window.onload = function () {
+    var trump;
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {
+        preload: preload,
+        create: create,
+        update: update
+    });
 
-window.onload = function() {    
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
     
-    
-    
-    
+
     function preload() {
-            game.load.image('logo', './assets/img/phaser.png');
-            game.load.image('trump', './assets/img/trumphead.png')
-        }
+        game.load.image('logo', './assets/img/phaser.png');
+        game.load.image('trump', './assets/img/trumphead.png')
+    }
 
     function create() {
         // need to have this for physics
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        
+
+        // background
         var logo = game.add.sprite(
             game.world.centerX, game.world.centerY, 'logo');
         logo.anchor.setTo(0.5, 0.5);
-
-        game.trumpHead = game.add.sprite(game.world.centerX,
-                                       game.world.centerY,
-                                       'trump');
-        game.trumpHead.anchor.setTo(0.5, 0.5);
-        // objects need to have their physics enabled
-        game.physics.arcade.enable(game.trumpHead);
-        game.trumpHead.body.bounce.y = 0.2;
-        game.trumpHead.body.collideWorldBounds = true;
-
-    }
-    
-    function update() {
-    cursors = game.input.keyboard.createCursorKeys();
-
-    //  Reset the players velocity (movement)
-    game.trumpHead.body.velocity.x = 0;
-    game.trumpHead.body.velocity.y = 0;
-
-    if (cursors.left.isDown) {
-        //  Move to the left
-        game.trumpHead.body.velocity.x = -150;
-
-        //player.animations.play('left');
-    } else if (cursors.right.isDown) {
-        //  Move to the right
-        game.trumpHead.body.velocity.x = 150;
-
-        //player.animations.play('right');
-    }
-    
-    if (cursors.up.isDown) {
-        //  Move to the left
-        game.trumpHead.body.velocity.y = -150;
-
-        //player.animations.play('left');
-    } else if (cursors.down.isDown) {
-        //  Move to the right
-        game.trumpHead.body.velocity.y = 150;
-
-        //player.animations.play('right');
-    } else {
-        //  Stand still
-        //player.animations.stop();
-
-        //player.frame = 4;
-    }
         
+        // Set up our moveable trump
+        trump = game.add.sprite(
+        game.world.centerX, game.world.centerY, 'trump');
+        trump.anchor.setTo(0.5, 0.5);
+
+
+        // objects need to have their physics enabled
+        game.physics.arcade.enable(trump);
+        trump.body.collideWorldBounds = true;
+        trump.body.mass = 10;
+        trump.body.maxVelocity.x = 150;
+        trump.body.maxVelocity.y = 150;
+    }
+
+    function update() {
+        moveTrump();
+    }
+    
+    function moveTrump(){
+        cursors = game.input.keyboard.createCursorKeys();
+
+        // Reset the players velocity (movement)
+        trump.body.acceleration.x = 0;
+        trump.body.acceleration.y = 0;
+        
+        if (cursors.left.isDown) {
+            //  Move to the left
+            trump.body.acceleration.x = -800;
+            if(trump.body.velocity.x > 0)
+                trump.body.velocity.x = 0;
+
+            //player.animations.play('left');
+        } else if (cursors.right.isDown) {
+            //  Move to the right
+            trump.body.acceleration.x = 800;
+            if(trump.body.velocity.x <0)
+                trump.body.velocity.x = 0
+            //player.animations.play('right');
+        }else{
+            var vx = trump.body.velocity.x;
+            if(vx < 40 && vx > -40)
+                trump.body.velocity.x = 0;
+            else if(vx > 0)
+                trump.body.acceleration.x = -900;
+            else
+                trump.body.acceleration.x = 900;
+        }
+
+        if (cursors.up.isDown) {
+            //  Move to the left
+            trump.body.acceleration.y = -800;
+            if(trump.body.velocity.y > 0)
+                trump.body.velocity.y = 0;
+
+            //player.animations.play('left');
+        } else if (cursors.down.isDown) {
+            //  Move to the right
+            trump.body.acceleration.y = 800;
+            if(trump.body.velocity.y < 0)
+                trump.body.velocity.y = 0;
+
+            //player.animations.play('right');
+        }else{
+            var vy = trump.body.velocity.y;
+            if(vy < 40 && vy > -40)
+                trump.body.velocity.y = 0;
+            else if(vy > 0)
+                trump.body.acceleration.y = -900;
+            else
+                trump.body.acceleration.y = 900;
+        }
     }
 };
